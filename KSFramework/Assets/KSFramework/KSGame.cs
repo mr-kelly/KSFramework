0f51415129dc6aecc811fab3cb2b040ad3d34f3d
mrkelly <chepy.v@gmail.com>
2016-06-18 13:09:17 +0800

    0.7.0版本号发布

--- KSFramework/Assets/KSFramework/KSGame.cs
+++ KSFramework/Assets/KSFramework/KSGame.cs
@@ -39,6 +39,11 @@ namespace KSFramework
     /// </summary>
     public abstract class KSGame : MonoBehaviour, IAppEntry
     {
+        /// <summary>
+        /// KSFramework版本号
+        /// </summary>
+        public string Version = "0.7.0";
+
         public static KSGame Instance { get; private set; }
 
         /// <summary>
