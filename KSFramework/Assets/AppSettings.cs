abe109aeea2ed3c68b3d669a2ff7b8d6b77c4ac5
mrkelly <chepy.v@gmail.com>
2016-08-08 14:43:02 +0800

    GameConfig配置，统一移动到GameConfig目录，文件名直接以+开头，简化配置的路径

--- KSFramework/Assets/AppSettings.cs
+++ KSFramework/Assets/AppSettings.cs
@@ -47,8 +47,8 @@ namespace AppSettings
                 {
                     _settingsList = new IReloadableSettings[]
                     { 
-                        GameConfigSettings.GetInstance(),
                         TestSettings.GetInstance(),
+                        GameConfigSettings.GetInstance(),
                     };
                 }
                 return _settingsList;
@@ -72,190 +72,6 @@ namespace AppSettings
 
     }
 
-
-	/// <summary>
-	/// Auto Generate for Tab File: "GameConfig+Base.bytes", "GameConfig+TSV.bytes"
-    /// No use of generic and reflection, for better performance,  less IL code generating
-	/// </summary>>
-    public partial class GameConfigSettings : IReloadableSettings
-    {
-		public static readonly string[] TabFilePaths = 
-        {
-            "GameConfig+Base.bytes", "GameConfig+TSV.bytes"
-        };
-        static GameConfigSettings _instance;
-        Dictionary<string, GameConfigSetting> _dict = new Dictionary<string, GameConfigSetting>();
-
-        /// <summary>
-        /// Trigger delegate when reload the Settings
-        /// </summary>>
-	    public static System.Action OnReload;
-
-        /// <summary>
-        /// Constructor, just reload(init)
-        /// When Unity Editor mode, will watch the file modification and auto reload
-        /// </summary>
-	    private GameConfigSettings()
-	    {
-        }
-
-        /// <summary>
-        /// Get the singleton
-        /// </summary>
-        /// <returns></returns>
-	    public static GameConfigSettings GetInstance()
-	    {
-            if (_instance == null) 
-            {
-                _instance = new GameConfigSettings();
-
-                _instance._ReloadAll(true);
-    #if UNITY_EDITOR
-                if (SettingModule.IsFileSystemMode)
-                {
-                    for (var j = 0; j < TabFilePaths.Length; j++)
-                    {
-                        var tabFilePath = TabFilePaths[j];
-                        SettingModule.WatchSetting(tabFilePath, (path) =>
-                        {
-                            if (path.Replace("\\", "/").EndsWith(path))
-                            {
-                                _instance.ReloadAll();
-                                Log.LogConsole_MultiThread("Reload success! -> " + path);
-                            }
-                        });
-                    }
-
-                }
-    #endif
-            }
-	        return _instance;
-	    }
-        
-        public int Count
-        {
-            get
-            {
-                return _dict.Count;
-            }
-        }
-
-        /// <summary>
-        /// Do reload the setting file: GameConfig, no exception when duplicate primary key
-        /// </summary>
-        public void ReloadAll()
-        {
-            _ReloadAll(false);
-        }
-
-        /// <summary>
-        /// Do reload the setting file: GameConfig
-        /// </summary>
-	    void _ReloadAll(bool throwWhenDuplicatePrimaryKey)
-        {
-            for (var j = 0; j < TabFilePaths.Length; j++)
-            {
-                var tabFilePath = TabFilePaths[j];
-                using (var tableFile = SettingModule.Get(tabFilePath, false))
-                {
-                    foreach (var row in tableFile)
-                    {
-                        var pk = GameConfigSetting.ParsePrimaryKey(row);
-                        GameConfigSetting setting;
-                        if (!_dict.TryGetValue(pk, out setting))
-                        {
-                            setting = new GameConfigSetting(row);
-                            _dict[setting.Id] = setting;
-                        }
-                        else 
-                        {
-                            if (throwWhenDuplicatePrimaryKey) throw new System.Exception(string.Format("DuplicateKey, Class: {0}, File: {1}, Key: {2}", this.GetType().Name, tabFilePath, pk));
-                            else setting.Reload(row);
-                        }
-                    }
-                }
-            }
-
-	        if (OnReload != null)
-	        {
-	            OnReload();
-	        }
-        }
-
-	    /// <summary>
-        /// foreachable enumerable: GameConfig
-        /// </summary>
-        public static IEnumerable GetAll()
-        {
-            foreach (var row in GetInstance()._dict.Values)
-            {
-                yield return row;
-            }
-        }
-
-        /// <summary>
-        /// GetEnumerator for `MoveNext`: GameConfig
-        /// </summary> 
-	    public static IEnumerator GetEnumerator()
-	    {
-	        return GetInstance()._dict.Values.GetEnumerator();
-	    }
-         
-	    /// <summary>
-        /// Get class by primary key: GameConfig
-        /// </summary>
-        public static GameConfigSetting Get(string primaryKey)
-        {
-            GameConfigSetting setting;
-            if (GetInstance()._dict.TryGetValue(primaryKey, out setting)) return setting;
-            return null;
-        }
-
-        // ========= CustomExtraString begin ===========
-        
-        // ========= CustomExtraString end ===========
-    }
-
-	/// <summary>
-	/// Auto Generate for Tab File: "GameConfig+Base.bytes", "GameConfig+TSV.bytes"
-    /// Singleton class for less memory use
-	/// </summary>
-	public partial class GameConfigSetting : TableRowParser
-	{
-		
-        /// <summary>
-        /// ID Column/编号/主键
-        /// </summary>
-        public string Id { get; private set;}
-        
-        /// <summary>
-        /// Name/名字
-        /// </summary>
-        public string Value { get; private set;}
-        
-
-        internal GameConfigSetting(TableRow row)
-        {
-            Reload(row);
-        }
-
-        internal void Reload(TableRow row)
-        { 
-            Id = row.Get_string(row.Values[0], ""); 
-            Value = row.Get_string(row.Values[1], ""); 
-        }
-
-        /// <summary>
-        /// Get PrimaryKey from a table row
-        /// </summary>
-        /// <param name="row"></param>
-        /// <returns></returns>
-        public static string ParsePrimaryKey(TableRow row)
-        {
-            var primaryKey = row.Get_string(row.Values[0], "");
-            return primaryKey;
-        }
-	}
 
 	/// <summary>
 	/// Auto Generate for Tab File: "Test.bytes"
@@ -427,6 +243,190 @@ namespace AppSettings
         { 
             Id = row.Get_string(row.Values[0], ""); 
             Value = row.Get_I18N(row.Values[1], ""); 
+        }
+
+        /// <summary>
+        /// Get PrimaryKey from a table row
+        /// </summary>
+        /// <param name="row"></param>
+        /// <returns></returns>
+        public static string ParsePrimaryKey(TableRow row)
+        {
+            var primaryKey = row.Get_string(row.Values[0], "");
+            return primaryKey;
+        }
+	}
+
+	/// <summary>
+	/// Auto Generate for Tab File: "GameConfig/+Base.bytes", "GameConfig/+TSV.bytes"
+    /// No use of generic and reflection, for better performance,  less IL code generating
+	/// </summary>>
+    public partial class GameConfigSettings : IReloadableSettings
+    {
+		public static readonly string[] TabFilePaths = 
+        {
+            "GameConfig/+Base.bytes", "GameConfig/+TSV.bytes"
+        };
+        static GameConfigSettings _instance;
+        Dictionary<string, GameConfigSetting> _dict = new Dictionary<string, GameConfigSetting>();
+
+        /// <summary>
+        /// Trigger delegate when reload the Settings
+        /// </summary>>
+	    public static System.Action OnReload;
+
+        /// <summary>
+        /// Constructor, just reload(init)
+        /// When Unity Editor mode, will watch the file modification and auto reload
+        /// </summary>
+	    private GameConfigSettings()
+	    {
+        }
+
+        /// <summary>
+        /// Get the singleton
+        /// </summary>
+        /// <returns></returns>
+	    public static GameConfigSettings GetInstance()
+	    {
+            if (_instance == null) 
+            {
+                _instance = new GameConfigSettings();
+
+                _instance._ReloadAll(true);
+    #if UNITY_EDITOR
+                if (SettingModule.IsFileSystemMode)
+                {
+                    for (var j = 0; j < TabFilePaths.Length; j++)
+                    {
+                        var tabFilePath = TabFilePaths[j];
+                        SettingModule.WatchSetting(tabFilePath, (path) =>
+                        {
+                            if (path.Replace("\\", "/").EndsWith(path))
+                            {
+                                _instance.ReloadAll();
+                                Log.LogConsole_MultiThread("Reload success! -> " + path);
+                            }
+                        });
+                    }
+
+                }
+    #endif
+            }
+	        return _instance;
+	    }
+        
+        public int Count
+        {
+            get
+            {
+                return _dict.Count;
+            }
+        }
+
+        /// <summary>
+        /// Do reload the setting file: GameConfig, no exception when duplicate primary key
+        /// </summary>
+        public void ReloadAll()
+        {
+            _ReloadAll(false);
+        }
+
+        /// <summary>
+        /// Do reload the setting file: GameConfig
+        /// </summary>
+	    void _ReloadAll(bool throwWhenDuplicatePrimaryKey)
+        {
+            for (var j = 0; j < TabFilePaths.Length; j++)
+            {
+                var tabFilePath = TabFilePaths[j];
+                using (var tableFile = SettingModule.Get(tabFilePath, false))
+                {
+                    foreach (var row in tableFile)
+                    {
+                        var pk = GameConfigSetting.ParsePrimaryKey(row);
+                        GameConfigSetting setting;
+                        if (!_dict.TryGetValue(pk, out setting))
+                        {
+                            setting = new GameConfigSetting(row);
+                            _dict[setting.Id] = setting;
+                        }
+                        else 
+                        {
+                            if (throwWhenDuplicatePrimaryKey) throw new System.Exception(string.Format("DuplicateKey, Class: {0}, File: {1}, Key: {2}", this.GetType().Name, tabFilePath, pk));
+                            else setting.Reload(row);
+                        }
+                    }
+                }
+            }
+
+	        if (OnReload != null)
+	        {
+	            OnReload();
+	        }
+        }
+
+	    /// <summary>
+        /// foreachable enumerable: GameConfig
+        /// </summary>
+        public static IEnumerable GetAll()
+        {
+            foreach (var row in GetInstance()._dict.Values)
+            {
+                yield return row;
+            }
+        }
+
+        /// <summary>
+        /// GetEnumerator for `MoveNext`: GameConfig
+        /// </summary> 
+	    public static IEnumerator GetEnumerator()
+	    {
+	        return GetInstance()._dict.Values.GetEnumerator();
+	    }
+         
+	    /// <summary>
+        /// Get class by primary key: GameConfig
+        /// </summary>
+        public static GameConfigSetting Get(string primaryKey)
+        {
+            GameConfigSetting setting;
+            if (GetInstance()._dict.TryGetValue(primaryKey, out setting)) return setting;
+            return null;
+        }
+
+        // ========= CustomExtraString begin ===========
+        
+        // ========= CustomExtraString end ===========
+    }
+
+	/// <summary>
+	/// Auto Generate for Tab File: "GameConfig/+Base.bytes", "GameConfig/+TSV.bytes"
+    /// Singleton class for less memory use
+	/// </summary>
+	public partial class GameConfigSetting : TableRowParser
+	{
+		
+        /// <summary>
+        /// ID Column/编号/主键
+        /// </summary>
+        public string Id { get; private set;}
+        
+        /// <summary>
+        /// Name/名字
+        /// </summary>
+        public string Value { get; private set;}
+        
+
+        internal GameConfigSetting(TableRow row)
+        {
+            Reload(row);
+        }
+
+        internal void Reload(TableRow row)
+        { 
+            Id = row.Get_string(row.Values[0], ""); 
+            Value = row.Get_string(row.Values[1], ""); 
         }
 
         /// <summary>
--- KSFramework/Assets/Plugins/KSFramework/Editor/KSFrameworkEditor.cs
+++ KSFramework/Assets/Plugins/KSFramework/Editor/KSFrameworkEditor.cs
@@ -173,12 +173,43 @@ return $UI_NAME
                 }
             }
         }
+
+        [MenuItem("KSFramework/UI/Reload Lua + Reload UI AssetBundle")]
+        public static void ReloadUI()
+        {
+            if (!EditorApplication.isPlaying)
+            {
+                Log.LogError("Reload UI only when your editor is playing!");
+                return;
+            }
+            foreach (var kv in UIModule.Instance.UIWindows)
+            {
+                var luaController = kv.Value.UIWindow as LuaUIController;
+                if (luaController) // 只处理LuaUIController
+                {
+                    var inOpenState = UIModule.Instance.IsOpen(kv.Key);
+                    if (inOpenState)
+                        UIModule.Instance.CloseWindow(kv.Key);
+
+                    luaController.ReloadLua();
+                    Log.LogWarning("Reload Lua - {0}", kv.Key);
+
+                    UIModule.Instance.ReloadWindow(kv.Key, (args, err) =>
+                    {
+                        if (inOpenState)
+                            UIModule.Instance.OpenWindow(kv.Key, luaController.LastOnOpenArgs);
+                    });
+
+                }
+            }
+            
+        }
         /// <summary>
         /// 找到所有的LuaUIController被进行Reload
         /// 如果Reload时，UI正在打开，将对其进行关闭，并再次打开，来立刻看到效果
         /// </summary>
         [MenuItem("KSFramework/UI/Reload Lua + ReOpen UI #%&r")]
-        public static void ReloadUI()
+        public static void ReloadUILua()
         {
             if (!EditorApplication.isPlaying)
             {
