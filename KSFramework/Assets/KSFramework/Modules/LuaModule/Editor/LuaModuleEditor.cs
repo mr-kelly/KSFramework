69530a05217549eb2f09df5bb60e4cec1babc731
qingqing-zhao <zhaoqignqing@gmail.com>
2018-09-10 23:29:40 +0800

    打包时对editorLuaScriptPath进行判空处理

--- KSFramework/Assets/KSFramework/Modules/LuaModule/Editor/LuaModuleEditor.cs
+++ KSFramework/Assets/KSFramework/Modules/LuaModule/Editor/LuaModuleEditor.cs
@@ -65,6 +65,11 @@ namespace KSFramework.Editor
                 var luaCount = 0;
                 var editorLuaScriptPath = Path.Combine(KResourceModule.EditorProductFullPath, luaPath);
                 editorLuaScriptPath = editorLuaScriptPath.Replace("\\", "/");
+                if (!Directory.Exists(editorLuaScriptPath))
+                {
+                    Debug.LogError("[LuaModuleEditor]lua script path not exist!");
+                    return;
+                }
                 var toDir = "Assets/StreamingAssets/" + luaPath;
 
                 // 所有的Lua脚本拷贝到StreamingAssets
