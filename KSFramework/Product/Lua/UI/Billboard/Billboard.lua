27d52e09b85f6ccff9bd2090d3363dc5698fe972
qingqing-zhao <zhaoqignqing@gmail.com>
2018-10-18 22:04:19 +0800

    rename

--- KSFramework/Product/Lua/UI/Billboard/Billboard.lua
+++ KSFramework/Product/Lua/UI/Billboard/Billboard.lua
@@ -19,7 +19,7 @@ function UIBillboard:OnOpen()
         rand = math.random(1,3)
     --    Cookie.Set('UIBillboard.RandomNumber', rand)
     --end
-    local billboardSetting = AppSettings.BillboardSettings.Get('Billboard' .. tostring(rand))
+    local billboardSetting = BillboardSettings.Get('Billboard' .. tostring(rand))
     -- self.TitleLabel.text = "This is a title"
     -- self.ContentLabel.text = "Here is content!"
     self.TitleLabel.text = billboardSetting.Title
