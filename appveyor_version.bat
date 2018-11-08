600a60e563ff4279e1bced727a4d027585283bc3
qingqing-zhao <zhaoqignqing@gmail.com>
2018-11-08 14:46:50 +0800

    update appveyor

--- appveyor_version.bat
+++ appveyor_version.bat
@@ -1,7 +1,7 @@
 echo ^
 using System.Reflection; ^
 [assembly: AssemblyTitle("KSFrameowork (http://github.com/mr-kelly/KSFramework)")]^
-[assembly: AssemblyDescription("KSFramework - KEngine + SLua")]^
+[assembly: AssemblyDescription("KSFramework - KEngine + XLua")]^
 [assembly: AssemblyConfiguration("")]^
 [assembly: AssemblyCompany("Kelly")]^
 [assembly: AssemblyProduct("KSFramework")]^
@@ -15,5 +15,5 @@ namespace KSFramework^
     {^
         public static readonly string Version = "%appveyor_build_version%";^
     }^
-} > KSFramework/Assets/Plugins/KSFramework/KSFrameworkInfo.cs
+} > KSFramework/Assets/KSFrameworkInfo.cs
 
