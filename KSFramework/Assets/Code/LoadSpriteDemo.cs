4814b8d2ecb888b1728817148a1ec4d6d6b3f9d8
qingqing-zhao <zhaoqignqing@gmail.com>
2019-02-15 19:46:25 +0800

    增加加载单个Sprite的Demo

---
+++ KSFramework/Assets/Code/LoadSpriteDemo.cs
@@ -0,0 +1,42 @@
+﻿using KEngine;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadSpriteDemo : MonoBehaviour
+{
+    public Image targetImage;
+    public Dropdown dropDown;
+
+    public string[] iconNames = {"button_green", "button_red", "button_yellow" };
+    // Use this for initialization
+    void Start()
+    {
+        dropDown.options.Clear();
+        foreach (string iconName in iconNames)
+        {
+            Dropdown.OptionData optionData = new Dropdown.OptionData(iconName);
+            dropDown.options.Add(optionData);
+        }
+
+        dropDown.onValueChanged.AddListener((index) =>
+        {
+            var selectName = iconNames[index];
+            Log.Info("切换{0},{1}",selectName,index);
+            SpriteLoader.Load(string.Format("uiatlas/button/{0}.png", selectName), (isOk, loadSprite) =>
+         {
+             if (isOk)
+             {
+                 targetImage.sprite = loadSprite;
+                 targetImage.SetNativeSize();
+                 Log.Info("图片加载完成:{0}", selectName);
+             }
+         });
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ KSFramework/Product/Lua/UI/LoadUISprite/LoadUISprite.lua
@@ -0,0 +1,22 @@
+
+local UIBase = import('UI/UIBase')
+
+local UILoadUISprite = {}
+extends(UILoadUISprite, UIBase)
+
+-- create a ui instance
+function UILoadUISprite.New(controller)
+    local newUI = new(UILoadUISprite)
+    newUI.Controller = controller
+    return newUI
+end
+
+function UILoadUISprite:OnInit(controller)
+    Log.Info('UILoadUISprite OnInit, do controls binding')
+end
+
+function UILoadUISprite:OnOpen()
+    Log.Info('UILoadUISprite OnOpen, do your logic')
+end
+
+return UILoadUISprite
