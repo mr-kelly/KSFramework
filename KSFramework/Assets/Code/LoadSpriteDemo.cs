75239b55e79f8fb5c3561cec39b5b9d0184332f9
qingqing-zhao <zhaoqignqing@gmail.com>
2019-02-22 21:32:15 +0800

    增加KSpriteAtlasLoader，用于加载SpriteAtlas

--- KSFramework/Assets/Code/LoadSpriteDemo.cs
+++ KSFramework/Assets/Code/LoadSpriteDemo.cs
@@ -1,4 +1,5 @@
-﻿using KEngine;
+﻿using System.Collections.Generic;
+using KEngine;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,23 +7,30 @@ public class LoadSpriteDemo : MonoBehaviour
 {
     public Image targetImage;
     public Dropdown dropDown;
+    public Dropdown dropDownAtlas;
+    public Button btnRelease, btnReleaseAtlas;
+    private KSpriteAtlasLoader atlasLoader;
 
-    public string[] iconNames = {"button_green", "button_red", "button_yellow" };
+    private Dictionary<string, SpriteLoader> spriteLoaders = new Dictionary<string, SpriteLoader>();
+    public string[] iconNames = { "button_green", "button_red", "button_yellow" };
+   
     // Use this for initialization
     void Start()
     {
         dropDown.options.Clear();
+        dropDownAtlas.options.Clear();
         foreach (string iconName in iconNames)
         {
             Dropdown.OptionData optionData = new Dropdown.OptionData(iconName);
             dropDown.options.Add(optionData);
+            dropDownAtlas.options.Add(optionData);
         }
 
         dropDown.onValueChanged.AddListener((index) =>
         {
             var selectName = iconNames[index];
-            Log.Info("切换{0},{1}",selectName,index);
-            SpriteLoader.Load(string.Format("uiatlas/button/{0}.png", selectName), (isOk, loadSprite) =>
+            Log.Info("切换{0},{1}", selectName, index);
+            var spriteLoader = SpriteLoader.Load(string.Format("uiatlas/button/{0}.png", selectName), (isOk, loadSprite) =>
          {
              if (isOk)
              {
@@ -31,12 +39,54 @@ public class LoadSpriteDemo : MonoBehaviour
                  Log.Info("图片加载完成:{0}", selectName);
              }
          });
+            if (!spriteLoaders.ContainsKey(spriteLoader.Path))
+            {
+                spriteLoaders.Add(spriteLoader.Path, spriteLoader);
+            }
+        });
+
+        //从spriteAtlas加载
+        dropDownAtlas.onValueChanged.AddListener((index) =>
+        {
+            var selectName = iconNames[index];
+            Log.Info("切换{0},{1}", selectName, index);
+            var atlasPath = "uiatlas/buttonatlas.spriteatlas";
+            atlasLoader = KSpriteAtlasLoader.Load(atlasPath, selectName, (isOk, loadSprite) =>
+            {
+                if (isOk)
+                {
+                    targetImage.sprite = loadSprite;
+                    targetImage.SetNativeSize();
+                    Log.Info("图片加载完成:{0}", selectName);
+
+                }
+            });
+        });
+        btnReleaseAtlas.onClick.AddListener(RelaseAtlasLoader);
+        btnRelease.onClick.AddListener(RelaseSpriteLoader);
+    }
+
+    public void RelaseSpriteLoader()
+    {
+        KAsync.Start().WaitForSeconds(5).Then(() =>
+        {
+            foreach (KeyValuePair<string, SpriteLoader> keyValuePair in spriteLoaders)
+            {
+                keyValuePair.Value.Release(true);
+            }
+
+            Destroy(targetImage);
+            KResourceModule.Collect();
         });
     }
 
-    // Update is called once per frame
-    void Update()
+    public void RelaseAtlasLoader()
     {
-
+        KAsync.Start().WaitForSeconds(5).Then(() =>
+        {
+            atlasLoader.Release(true);
+            Destroy(targetImage);
+            KResourceModule.Collect();
+        });
     }
 }
--- KSFramework/Assets/KSFramework/KEngine/KEngine/CoreModules/ResourceModule/KSpriteAtlasLoader.cs
+++ KSFramework/Assets/KSFramework/KEngine/KEngine/CoreModules/ResourceModule/KSpriteAtlasLoader.cs
@@ -1,9 +1,67 @@
-﻿using UnityEngine;
+﻿#if UNITY_2017_1_OR_NEWER
+using UnityEngine;
+using UnityEngine.U2D;
 
 namespace KEngine
 {
-    //TODO 加载spriteAtlas
-    public class KSpriteAtlasLoader //: AbstractResourceLoader
+    // 加载spriteAtlas
+    public class KSpriteAtlasLoader : AbstractResourceLoader
     {
+        public SpriteAtlas Asset
+        {
+            get { return ResultObject as SpriteAtlas; }
+        }
+
+        public delegate void CKSpriteAtlasLoaderDelegate(bool isOk, Sprite tex);
+
+        private AssetFileLoader AssetFileBridge;
+
+        public override float Progress
+        {
+            get { return AssetFileBridge.Progress; }
+        }
+
+        public string Path { get; private set; }
+
+        public static KSpriteAtlasLoader Load(string path, string spriteName, CKSpriteAtlasLoaderDelegate callback = null)
+        {
+            LoaderDelgate newCallback = null;
+            if (callback != null)
+            {
+                newCallback = (isOk, obj) =>
+                {
+                    SpriteAtlas spriteAtlas = obj as SpriteAtlas;
+                    if (spriteAtlas != null)
+                    {
+                        var sp = spriteAtlas.GetSprite(spriteName);
+                        callback(isOk, sp);
+                    }
+                    else
+                    {
+                        callback(isOk, null);
+                    }
+                };
+            }
+            return AutoNew<KSpriteAtlasLoader>(path, newCallback);
+        }
+
+        protected override void Init(string url, params object[] args)
+        {
+            base.Init(url, args);
+            Path = url;
+            AssetFileBridge = AssetFileLoader.Load(Path, OnAssetLoaded);
+        }
+
+        private void OnAssetLoaded(bool isOk, UnityEngine.Object obj)
+        {
+            OnFinish(obj);
+        }
+
+        protected override void DoDispose()
+        {
+            base.DoDispose();
+            AssetFileBridge.Release(); // all, Texture is singleton!
+        }
     }
 }
+#endif
