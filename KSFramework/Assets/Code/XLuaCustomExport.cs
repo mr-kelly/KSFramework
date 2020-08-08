0d85cd8231c3982da920e5c2f740ea09907b7ef5
qingqing-zhao <zhaoqignqing@gmail.com>
2020-08-08 14:12:12 +0800

    把其它库注册的移到后面

--- KSFramework/Assets/Code/XLuaCustomExport.cs
+++ KSFramework/Assets/Code/XLuaCustomExport.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Text;
 //using DG.Tweening;
 using KEngine;
 using KEngine.UI;
@@ -18,52 +16,6 @@ using XLua;
 /// </summary>
 public static class XLuaCustomExport
 {
-    /// <summary>
-    /// dotween的扩展方法在lua中调用
-    /// </summary>
-	  /*
-	[LuaCallCSharp]
-    [ReflectionUse]
-    public static List<Type> dotween_lua_call_cs_list = new List<Type>()
-    {
-        typeof(DG.Tweening.AutoPlay),
-        typeof(DG.Tweening.AxisConstraint),
-        typeof(DG.Tweening.Ease),
-        typeof(DG.Tweening.LogBehaviour),
-        typeof(DG.Tweening.LoopType),
-        typeof(DG.Tweening.PathMode),
-        typeof(DG.Tweening.PathType),
-        typeof(DG.Tweening.RotateMode),
-        typeof(DG.Tweening.ScrambleMode),
-        typeof(DG.Tweening.TweenType),
-        typeof(DG.Tweening.UpdateType),
-
-        typeof(DG.Tweening.DOTween),
-        typeof(DG.Tweening.DOVirtual),
-        typeof(DG.Tweening.EaseFactory),
-        typeof(DG.Tweening.Tweener),
-        typeof(DG.Tweening.Tween),
-        typeof(DG.Tweening.Sequence),
-        typeof(DG.Tweening.TweenParams),
-        typeof(DG.Tweening.Core.ABSSequentiable),
-
-        typeof(DG.Tweening.Core.TweenerCore<Vector3, Vector3, DG.Tweening.Plugins.Options.VectorOptions>),
-
-        typeof(DG.Tweening.TweenCallback),
-        typeof(DG.Tweening.TweenExtensions),
-        typeof(DG.Tweening.TweenSettingsExtensions),
-        typeof(DG.Tweening.ShortcutExtensions),
-        typeof(DG.Tweening.ShortcutExtensions43),
-        typeof(DG.Tweening.ShortcutExtensions46),
-        typeof(DG.Tweening.ShortcutExtensions50),
-       
-        //在生成xlua的代码时以下会报错
-        //typeof(DG.Tweening.DOTweenPath),
-        //typeof(DG.Tweening.DOTweenVisualManager),
-    }
-	**/
-
-
     [LuaCallCSharp]
     public static List<Type> LuaCallCSharpUI
     {
@@ -146,6 +98,7 @@ public static class XLuaCustomExport
     {
         typeof(System.Action<GameObject>),
         typeof(System.Action<string, string, PointerEventData>),
+        typeof(System.Action<bool>),
     };
     
     [ReflectionUse]
@@ -332,4 +285,53 @@ public static class XLuaCustomExport
 
         return null;
     }
+
+    #region 其它库的注册
+
+    /// <summary>
+    /// dotween的扩展方法在lua中调用
+    /// </summary>
+    /*
+  [LuaCallCSharp]
+  [ReflectionUse]
+  public static List<Type> dotween_lua_call_cs_list = new List<Type>()
+  {
+      typeof(DG.Tweening.AutoPlay),
+      typeof(DG.Tweening.AxisConstraint),
+      typeof(DG.Tweening.Ease),
+      typeof(DG.Tweening.LogBehaviour),
+      typeof(DG.Tweening.LoopType),
+      typeof(DG.Tweening.PathMode),
+      typeof(DG.Tweening.PathType),
+      typeof(DG.Tweening.RotateMode),
+      typeof(DG.Tweening.ScrambleMode),
+      typeof(DG.Tweening.TweenType),
+      typeof(DG.Tweening.UpdateType),
+
+      typeof(DG.Tweening.DOTween),
+      typeof(DG.Tweening.DOVirtual),
+      typeof(DG.Tweening.EaseFactory),
+      typeof(DG.Tweening.Tweener),
+      typeof(DG.Tweening.Tween),
+      typeof(DG.Tweening.Sequence),
+      typeof(DG.Tweening.TweenParams),
+      typeof(DG.Tweening.Core.ABSSequentiable),
+
+      typeof(DG.Tweening.Core.TweenerCore<Vector3, Vector3, DG.Tweening.Plugins.Options.VectorOptions>),
+
+      typeof(DG.Tweening.TweenCallback),
+      typeof(DG.Tweening.TweenExtensions),
+      typeof(DG.Tweening.TweenSettingsExtensions),
+      typeof(DG.Tweening.ShortcutExtensions),
+      typeof(DG.Tweening.ShortcutExtensions43),
+      typeof(DG.Tweening.ShortcutExtensions46),
+      typeof(DG.Tweening.ShortcutExtensions50),
+     
+      //在生成xlua的代码时以下会报错
+      //typeof(DG.Tweening.DOTweenPath),
+      //typeof(DG.Tweening.DOTweenVisualManager),
+  }
+  **/
+
+    #endregion
 }
