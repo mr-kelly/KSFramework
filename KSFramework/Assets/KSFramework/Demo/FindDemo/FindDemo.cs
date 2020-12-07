e961ee7f49e0158cad40a22cfbd1a60034213ecb
qingqing-zhao <zhaoqignqing@gmail.com>
2020-12-07 19:52:10 +0800

    FindDemo 修改

--- KSFramework/Assets/KSFramework/Demo/FindDemo/FindDemo.cs
+++ KSFramework/Assets/KSFramework/Demo/FindDemo/FindDemo.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using KEngine;
 using UnityEngine;
@@ -6,8 +7,13 @@ using UnityEngine.UI;
 
 public class FindDemo : MonoBehaviour
 {
+    private void Start()
+    {
+        Application.targetFrameRate = 30;
+    }
+
     // Start is called before the first frame update
-    void Start()
+    void OnEnable()
     {
         TestFind();
     }
@@ -18,23 +24,23 @@ public class FindDemo : MonoBehaviour
         if (btn_1)
         {
             btn_1.onClick.RemoveAllListeners();
-            btn_1.onClick.AddListener(() =>
-            {
-                TestFind();
-            });
+            btn_1.onClick.AddListener(() => { });
         }
-        Log.Info("find btn_1:{0}",btn_1 );
         
+        Log.Info("find btn_1:{0}", btn_1);
+
         var btn_xxx = gameObject.FindChild<Button>("btn_xxx");
-        Log.Info("find btn_xxx:{0}",btn_xxx);
+        Log.Info("find btn_xxx:{0}", btn_xxx);
         
         var text = gameObject.FindChild<Text>("Text");
-        Log.Info("find Text:{0} ,Text.text={1}",text,text?.text);
-        
-        var text_name = gameObject.FindChild<Text>("text_name");
-        Log.Info("find text_name:{0} ,Text.text={1}",text_name,text_name?.text);
-        
+        Log.Info("find Text:{0} ,Text.text={1}", text, text?.text);
+
+        KProfiler.BeginWatch("Find-Queue");
+        var text_name = gameObject.FindChild<Text>("text_name", false, false);
+        Log.Info("find text_name:{0} ,Text.text={1}", text_name, text_name?.text);
+        KProfiler.EndWatch("Find-Queue");
         var text_test = gameObject.FindChild<Button>("text_name");
-        
     }
+
+
 }
