363151a06d09ff023e0a50ee7f2d7802d1fc2c00
qingqing-zhao <zhaoqignqing@gmail.com>
2020-12-08 14:50:11 +0800

    fixed call gui error

--- KSFramework/Assets/KSFramework/KEngine/Editor/KEngine.Editor/KQuickWindowEditor.cs
+++ KSFramework/Assets/KSFramework/KEngine/Editor/KEngine.Editor/KQuickWindowEditor.cs
@@ -10,37 +10,35 @@ using UnityEditor;
 public class KQuickWindowEditor : EditorWindow
 {
     Vector3 scrollPos = Vector2.zero;
-    [SerializeField]
-    private string reloadUIAb;
-    [SerializeField]
-    private string reloadUIScript;
+    [SerializeField] private string reloadUIAb;
+    [SerializeField] private string reloadUIScript;
     private GUIStyle textFieldStyle;
     private bool init = false;
-	
+
     [MenuItem("KEngine/Open Quick Window %&Q")]
     static void DoIt()
     {
-       
-        //note 在Windows Editor下Scree.Width,Screen.Height获取的并不是真实的显示器宽度和高度
+        //note 在Editor下Scree.Width,Screen.Height获取的并不是真实的显示器宽度和高度
         KQuickWindowEditor window = EditorWindow.GetWindow<KQuickWindowEditor>();
         window.titleContent = new GUIContent("快捷工具窗");
         window.position = new Rect(400, 100, 640, 480);
-        window.InitStyle();
         window.Show();
     }
 
     void InitStyle()
     {
-        if (init) return;
-        textFieldStyle = new GUIStyle (GUI.skin.textField);
+        if (init)
+            return;
+        textFieldStyle = new GUIStyle(GUI.skin.textField);
         textFieldStyle.alignment = TextAnchor.MiddleLeft;
-        
+
         init = true;
     }
-    
+
     public void OnGUI()
     {
-        scrollPos = GUI.BeginScrollView(new Rect(10, 10, position.width, position.height),scrollPos, new Rect(0, 0, 360, 1000));
+        InitStyle();
+        scrollPos = GUI.BeginScrollView(new Rect(10, 10, position.width, position.height), scrollPos, new Rect(0, 0, 360, 1000));
         DrawKEngineInit();
         GUILayout.Space(20);
 
@@ -87,47 +85,54 @@ public class KQuickWindowEditor : EditorWindow
             //TODO 仅仅是重载编译后的配置表
             //SettingsManager.AllSettingsReload();
         }
+
         GUILayout.EndHorizontal();
-        
+
         GUILayout.BeginHorizontal();
         if (GUILayout.Button("快速编译配置表", GUILayout.ExpandWidth(true), GUILayout.MaxHeight(30)))
         {
             SettingModuleEditor.QuickCompileSettings();
         }
+
         if (GUILayout.Button("编译配置表并生成代码", GUILayout.ExpandWidth(true), GUILayout.MaxHeight(30)))
         {
             SettingModuleEditor.CompileSettings();
         }
+
         GUILayout.EndHorizontal();
-        
+
         GUILayout.BeginHorizontal();
         if (GUILayout.Button("重载所有UI的Lua代码", GUILayout.ExpandWidth(true), GUILayout.MaxHeight(30)))
         {
             KSFrameworkEditor.ReloadAllUIScript();
         }
+
         if (GUILayout.Button("重新打开所有UI", GUILayout.ExpandWidth(true), GUILayout.MaxHeight(30)))
         {
             KSFrameworkEditor.ReloadAllUI();
         }
+
         GUILayout.EndHorizontal();
-        
+
         GUILayout.Space(10);
         GUILayout.BeginHorizontal();
-        EditorGUILayout.LabelField("UI名字:",GUILayout.Width(40));
-        reloadUIScript = EditorGUILayout.TextField(reloadUIScript,GUILayout.MinWidth(120));
+        EditorGUILayout.LabelField("UI名字:", GUILayout.Width(40));
+        reloadUIScript = EditorGUILayout.TextField(reloadUIScript, GUILayout.MinWidth(120));
         if (GUILayout.Button("重载Lua脚本", GUILayout.MinWidth(100), GUILayout.MaxHeight(30)))
         {
             KSFrameworkEditor.ReloadUIScript(reloadUIScript);
         }
+
         GUILayout.EndHorizontal();
         GUILayout.Space(10);
         GUILayout.BeginHorizontal();
-        EditorGUILayout.LabelField("UI名字:",GUILayout.Width(40));
-        reloadUIAb = EditorGUILayout.TextField(reloadUIAb,GUILayout.MinWidth(120));
+        EditorGUILayout.LabelField("UI名字:", GUILayout.Width(40));
+        reloadUIAb = EditorGUILayout.TextField(reloadUIAb, GUILayout.MinWidth(120));
         if (GUILayout.Button("重载AB并打开", GUILayout.MinWidth(100), GUILayout.MaxHeight(30)))
         {
             KSFrameworkEditor.ReloadUIAB(reloadUIAb);
         }
+
         GUILayout.EndHorizontal();
         //GUILayout.Space(10);
     }
@@ -143,10 +148,12 @@ public class KQuickWindowEditor : EditorWindow
         {
             KUGUIBuilder.CreateNewUI();
         }
+
         if (GUILayout.Button("为当前UI创建Lua脚本", GUILayout.ExpandWidth(true), GUILayout.MaxHeight(30)))
         {
-             KSFrameworkEditor.AutoMakeUILuaScripts();
+            KSFrameworkEditor.AutoMakeUILuaScripts();
         }
+
         GUILayout.EndHorizontal();
 
 
@@ -155,10 +162,12 @@ public class KQuickWindowEditor : EditorWindow
         {
             KUGUIBuilder.ExportCurrentUI();
         }
+
         if (GUILayout.Button("导出全部UI", GUILayout.ExpandWidth(true), GUILayout.MaxHeight(30)))
         {
             KUGUIBuilder.ExportAllUI();
         }
+
         GUILayout.EndHorizontal();
     }
 
@@ -172,10 +181,12 @@ public class KQuickWindowEditor : EditorWindow
         {
             BuildTools.BuildAllAssetBundles();
         }
+
         if (GUILayout.Button("全部删除并重新打包AB", GUILayout.ExpandWidth(true), GUILayout.MaxHeight(30)))
         {
             BuildTools.ReBuildAllAssetBundles();
         }
+
         GUILayout.EndHorizontal();
 
 
@@ -184,11 +195,13 @@ public class KQuickWindowEditor : EditorWindow
         {
             BuildTools.MakeAssetBundleNames();
         }
+
         if (GUILayout.Button("打开AB目录", GUILayout.ExpandWidth(true), GUILayout.MaxHeight(20)))
         {
-            var path = KResourceModule.ProductRelPath +"/Bundles/"+  KResourceModule.GetBuildPlatformName();
+            var path = KResourceModule.ProductRelPath + "/Bundles/" + KResourceModule.GetBuildPlatformName();
             OpenFolder(path);
         }
+
         GUILayout.EndHorizontal();
 
         GUILayout.BeginHorizontal();
@@ -198,7 +211,6 @@ public class KQuickWindowEditor : EditorWindow
         }
 
         GUILayout.EndHorizontal();
-
     }
 
     void DrawBuildUI()
@@ -212,10 +224,12 @@ public class KQuickWindowEditor : EditorWindow
         {
             KAutoBuilder.PerformWinReleaseBuild();
         }
+
         if (GUILayout.Button("打PC版-Dev", GUILayout.ExpandWidth(true), GUILayout.MaxHeight(20)))
         {
             KAutoBuilder.PerformWinBuild();
         }
+
         GUILayout.EndHorizontal();
 
         GUILayout.BeginHorizontal();
@@ -223,22 +237,25 @@ public class KQuickWindowEditor : EditorWindow
         {
             KAutoBuilder.PerformAndroidBuild();
         }
+
         if (GUILayout.Button("打IOS版", GUILayout.ExpandWidth(true), GUILayout.MaxHeight(20)))
         {
             KAutoBuilder.PerformiOSBuild();
         }
+
         GUILayout.EndHorizontal();
-        
+
         GUILayout.BeginHorizontal();
         if (GUILayout.Button("打开安装包目录", GUILayout.ExpandWidth(true), GUILayout.MaxHeight(20)))
         {
             var path = KResourceModule.ProductRelPath + "/Apps/" + KResourceModule.GetBuildPlatformName();
-           OpenFolder(path);
+            OpenFolder(path);
         }
+
         GUILayout.EndHorizontal();
         GUILayout.BeginHorizontal();
-       
-        
+
+
         GUILayout.EndHorizontal();
     }
 
@@ -254,7 +271,7 @@ public class KQuickWindowEditor : EditorWindow
             KProfiler.Dump();
         }
 
-       
+
         GUILayout.EndHorizontal();
     }
 
@@ -268,8 +285,8 @@ public class KQuickWindowEditor : EditorWindow
             DirectoryInfo directoryInfo = new DirectoryInfo(fullPath);
             if (directoryInfo.Parent != null) fullPath = directoryInfo.Parent.FullName;
         }
+
         Log.Debug("open: {0}", fullPath);
         System.Diagnostics.Process.Start("explorer.exe", fullPath);
     }
-    
 }
