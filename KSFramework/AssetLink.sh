02dba4941ab5dd1a486c0d24465d6112390b7681
qingqing-zhao <zhaoqignqing@gmail.com>
2021-05-08 15:59:11 +0800

    fixed: mac os link by 1159281

--- KSFramework/AssetLink.sh
+++ KSFramework/AssetLink.sh
@@ -2,5 +2,5 @@
 rm -rf ./Assets/StreamingAssets/Lua
 rm -rf ./Assets/StreamingAssets/Setting
 
-ln -s Assets/StreamingAssets/Lua/ ./../Product/Lua/
-ln -s Assets/StreamingAssets/Setting/ ./../Product/Setting/
+ln -s ./Product/Lua/ ./Assets/StreamingAssets/
+ln -s ./Product/Setting/ ./Assets/StreamingAssets/
